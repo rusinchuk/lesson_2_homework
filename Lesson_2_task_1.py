961908026917aa78a861fc388c812ebd514d98a3
srusinchuk <srusinchuk@b2bsoft.com>
2021-03-25 23:50:22 +0200

    Homework Lesson_2_task_1 done. @elpiankova

---
+++ Lesson_2_task_1.py
@@ -0,0 +1,35 @@
+s = 'dsadlsd1324353.4343553.dsdsfdfdf dfdffd 234353535'
+print(s.isdigit())
+print(s.count(' '))
+print(s.count('.'))
+
+s = 'Homework'
+s = (46 * ' ') + 'Homework' + (46 * ' ')
+print(s)
+print(len(s))
+
+s = 'dsadlsd1324353.4343553.dsdsfdfdf dfdffd 234353535'
+s = (s.capitalize())
+print (s.capitalize())
+print (s.index('ing',-1))
+print (s.find('a'))
+print (s.split (' '))
+s = '   dsadlsd1324353.4343553.dsdsfdfdf dfdffd 234353535   '
+print (s.strip())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
