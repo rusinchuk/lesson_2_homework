ffd48e677c156e28a3e25a12093f841ee5e73e46
srusinchuk <srusinchuk@b2bsoft.com>
2021-03-26 16:33:52 +0200

    Homework_lesson_2_task_3 done. @elpiankova

---
+++ Lesson_2_task_3.py
@@ -0,0 +1,24 @@
+a = ['Grey', 'Yellow', 'Brown', 'Black', 'White']
+print(a[-3])
+#2. Выведите 1-ю букву второго слова из списка. - не нашел способа как реализовать
+#3. Выведите последнюю букву последнего слова из списка - не нашел способа как реализовать
+a.append('Green')
+print(a)
+a.insert(3, 'Blue')
+print(a)
+a.remove('Grey')
+print(a)
+a.remove('world')
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
