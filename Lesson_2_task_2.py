71be1c887fc6a36257246e6808c19cd69dfe9cd5
srusinchuk <srusinchuk@b2bsoft.com>
2021-03-26 13:36:06 +0200

    Homework_lesson_2_task_2 done. @elpiankova

---
+++ Lesson_2_task_2.py
@@ -0,0 +1,25 @@
+q = 367 ** 2 + 127 ** 2
+print(q ** .5)
+
+n = 98
+print(n // 10 % 10)
+
+a = 358
+print(sum(map(int, str(a))))
+
+s = 958
+print(s + 2 - (s % 2))
+
+s = int(input(18.9))
+print(s + 2 - (s % 2))
+
+x = 25.98
+print(x - int(x))
+
+z = 36.1487
+print(int(x * 10) % 10)
+
+
+
+
+
